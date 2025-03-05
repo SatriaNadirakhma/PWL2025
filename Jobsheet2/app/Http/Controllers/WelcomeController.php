ad93bfcfcb6272a68fe38b43ef4384f300c8f566
SatriaNadiRakhma <rakhmadanisatria@gmail.com>
2025-03-05 22:09:35 +0700

    Jobsheet-2

---
+++ Jobsheet2/app/Http/Controllers/WelcomeController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class WelcomeController extends Controller
+{
+    public function hello() {
+        return ('Hello World');
+        }
+
+    // public function greeting(){
+    //     return view('blog.hello', ['name' => 'Satria R.']);
+    //     }
+
+    public function greeting(){ 
+        return view('blog.hello')
+        ->with('name','Satria')
+        ->with('occupation','Astronaut');
+    }
+          
+}
+
